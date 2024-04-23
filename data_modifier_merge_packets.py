9b4230c0059744f3312403dd86cb1b04fc5df72b
KamiYuri <vietdunghoang.hust@gmail.com>
2024-04-23 13:23:42 +0700

    update

--- data_modifier_merge_packets.py
+++ data_modifier_merge_packets.py
@@ -1,17 +1,16 @@
-from scapy.all import *
-from scapy.layers.tls.all import *
-from enum import Enum
-
 import argparse
 import binascii
-import glob
-import json
-import os
-import pandas as pd
+from enum import Enum
+from pathlib import Path
 
-YOUR_DELAY_THRESHOLD = 40
+import pandas as pd
+from scapy.all import *
+from scapy.layers.tls.all import *
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
 PAYLOAD_LENGTH_THRESHOLD = 512
-PACKETS_NUM_THRESHOLD = 20
+PACKETS_NUM_THRESHOLD = 10
 
 
 class ModifierType(Enum):
@@ -20,43 +19,41 @@ class ModifierType(Enum):
 
 
 class Label(Enum):
-    BENIGN = 0
-    DNS2TCp = 1
-    IODINE = 2
-    DNSCAT2 = 3
+    BENIGN = 'benign'
+    DNS2TCP = 'dns2tcp'
+    IODINE = 'iodine'
+    DNSCAT2 = 'dnscat2'
 
 
 class DataModifier:
-    def __init__(self, output: str, output_type: ModifierType, label: Label):
+    def __init__(self, output: Path, output_type: ModifierType, label: str):
         self.label = label
         self.output = output
         self.output_type = output_type
-        self.pcap_list = None
+        self.pcaps_list = None
 
-    def run(self, pcap_files_dir: str):
-        # Init output file
-        self.init_output()
-
+    def run(self, pcap_files_dir: Path):
         # Load the pcap files
         self.load_pcap_files(pcap_files_dir)
         self.read_pcap_file()
 
-    def load_pcap_files(self, directory: str):
+    def load_pcap_files(self, pcap_files_dir: Path):
         # Load the pcap files
-        self.pcap_list = glob.glob(os.path.join(directory, '*.pcap'))
+        self.pcaps_list = list(pcap_files_dir.glob('*.pcap'))
 
     def read_pcap_file(self):
         # Parse the pcap files
         flow_id = 1
-        for file in self.pcap_list:
-            # Read the pcap file
-            packets = rdpcap(file)
-            if len(packets) < PACKETS_NUM_THRESHOLD:
-                continue
+        for file in self.pcaps_list:
+            print(f"Processing {file}")
 
-            data_row = []
-            payload_old = []
+            # Read the pcap file
+            single_flow_packets = list()
+            payload_old = list()
+            packets = rdpcap(str(Path(file).resolve()))
             for packet in packets:
+                if len(single_flow_packets) >= PACKETS_NUM_THRESHOLD:
+                    break
                 if packet.haslayer(TLSApplicationData):
                     # Extract the data
                     payload_new = self.extract_pcap_files(packet)
@@ -64,44 +61,35 @@ class DataModifier:
                     if len(payload_new) > PAYLOAD_LENGTH_THRESHOLD:
                         # If the previous payload is not empty, then append it to the payloads list
                         if payload_old:
-                            data_row.append(payload_old)
-                            payload_old = []
+                            single_flow_packets.append([flow_id] + payload_old + [self.label])
+                            payload_old = list()
 
                         # Just take enough bytes to make the total length equal to PAYLOAD_LENGTH_THRESHOLD
-                        data_row.append(payload_new[: PAYLOAD_LENGTH_THRESHOLD])
+                        single_flow_packets.append([flow_id] + payload_new[:PAYLOAD_LENGTH_THRESHOLD] + [self.label])
                     # If the payload length is less than PAYLOAD_LENGTH_THRESHOLD
                     else:
                         # Check whether new payload can be merged with the previous payload without the total length
                         # exceeding PAYLOAD_LENGTH_THRESHOLD
-                        if len(payload_new) + len(payload_old) < PAYLOAD_LENGTH_THRESHOLD:
+                        if len(payload_new) + len(payload_old) <= PAYLOAD_LENGTH_THRESHOLD:
                             # Merge the new payload with the previous payload
                             payload_old.extend(payload_new)
                         # If the total length exceeds PAYLOAD_LENGTH_THRESHOLD, add old payload to the payloads list
                         else:
                             # If the previous payload is not empty, then append it to the payloads list
                             if payload_old:
-                                data_row.append(payload_old)
+                                single_flow_packets.append([flow_id] + payload_old + [self.label])
 
                             payload_old = payload_new
 
-                # Check if the number of packets is greater than or equal to 20
-                if len(data_row) >= 20:
-                    break
-
-            # If the number of rows is less than 20, then skip this flow
-            if len(data_row) < 20:
-                continue
-
-            # Take only the first 20 rows
-            data_row = data_row[:20]
-
-            # Add padding to the data_row
-            data_row = [
-                [flow_id] + item + [0] * (PAYLOAD_LENGTH_THRESHOLD - len(item)) + [self.label.value] for item in data_row]
-
-            self.write(data_row)
+            # If the number of packets is less than the threshold, then add padding packets
+            if len(single_flow_packets) < PACKETS_NUM_THRESHOLD:
+                for i in range(PACKETS_NUM_THRESHOLD - len(single_flow_packets)):
+                    single_flow_packets.append([flow_id] + [0] * 512 + [self.label])
 
             flow_id += 1
+            self.write(single_flow_packets)
+
+        print(f"Total {flow_id - 1} flows")
 
     @staticmethod
     def extract_pcap_files(packet: Packet):
@@ -112,42 +100,66 @@ class DataModifier:
         return [int(hex_data[i:i + 2], 16) for i in range(0, len(hex_data), 2)]
 
     def write(self, data):
-        df = pd.DataFrame(data=data,
-                          columns=["flow_id"] + [f'byte_{i + 1}' for i in range(512)] + ["label"])
+        df = pd.DataFrame(data=data)
 
         if self.output_type == ModifierType.CSV:
             df.to_csv(self.output, index=False, mode='a', header=False)
         elif self.output_type == ModifierType.FEATHER:
             df.to_feather(self.output)
 
-    def init_output(self):
-        # Generate header
-        headers = ["flow_id"] + [f'byte_{i + 1}' for i in range(512)] + ["label"]
-        df = pd.DataFrame(columns=headers)
 
-        if self.output_type == ModifierType.CSV:
-            df.to_csv(self.output, index=False)
-        elif self.output_type == ModifierType.FEATHER:
-            df.to_feather(self.output)
+def init_output_header(output: Path, output_type: ModifierType):
+    # Generate header
+    headers = ["flow_id"]
+    headers = headers + [f'byte_{i + 1}' for i in range(512)]
+    headers = headers + ["label"]
+    df = pd.DataFrame(columns=headers)
+
+    # Write the header to the output file
+    if output_type == ModifierType.CSV:
+        df.to_csv(output, index=False)
+    elif output_type == ModifierType.FEATHER:
+        df.to_feather(output)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Parse pcap files and generate a dataset')
+    dev = True
 
-    parser.add_argument('-i', '--pcap_files_dir', type=str, required=True,
-                        help='Input directory containing pcap files that need to be parsed')
-    parser.add_argument('-o', '--output', type=str, required=True, help='Path of the output file')
-    parser.add_argument('-l', '--label', type=int, choices=[item.value for item in Label.__members__.values()],
-                        required=True,
-                        help='Flag of data: \n 0 for benign \n 1 for dns2tcp \n 2 for iodine \n 3 for dnscat2')
-    parser.add_argument('-m', '--mode', type=str, choices=['csv', 'feather'], required=True,
-                        help='Output mode: csv or feather')
+    if dev:
+        output_path = Path('datasets/output.csv')
+        input_dir = Path('datasets/pcaps')
+        mode = ModifierType.CSV
+        label = Label.DNS2TCP
+    else:
+        parser = argparse.ArgumentParser(description='Parse pcap files and generate a dataset')
 
-    args = parser.parse_args()
+        parser.add_argument('-i', '--input_dir', type=str, required=True,
+                            help='Input directory containing pcap files that need to be parsed')
+        parser.add_argument('-o', '--output_path', type=str, required=True, help='Path of the output file')
+        parser.add_argument('-l', '--label', type=str, choices=[item.value for item in Label.__members__.values()],
+                            required=True,
+                            help='Flag of data: \n 0 for benign \n 1 for dns2tcp \n 2 for iodine \n 3 for dnscat2')
+        parser.add_argument('-m', '--mode', type=str, choices=['csv', 'feather'], required=True,
+                            help='Output mode: csv or feather')
 
-    mode = ModifierType.CSV if args.mode == 'csv' else ModifierType.FEATHER
+        args = parser.parse_args()
 
-    DataModifier(output=args.output, output_type=mode, label=Label(args.label)).run(args.pcap_files_dir)
+        output_path = Path(args.output_path)
+        input_dir = Path(args.input_dir)
+        mode = ModifierType.CSV if args.mode == 'csv' else ModifierType.FEATHER
+        label = Label(args.label)
 
-    # DataModifier(output='datasets/test_pcap/output.csv', output_type=ModifierType.CSV, label=Label.MALICIOUS,
-    # custom_headers={'type': 'test', 'sasd': 'asdasd'}).run("/home/bkcs/Documents/doh/pcaps/DoHBenign-NonDoH/test")
+    # Remove the existing file
+    if output_path.exists():
+        output_path.unlink()
+    # Create the file and write the header
+    output_path.touch()
+    init_output_header(output_path, ModifierType.CSV)
+
+    if not input_dir.exists():
+        print(f"Directory '{input_dir}' does not exist")
+        exit(1)
+
+    DataModifier(output=output_path, output_type=mode, label=label.value).run(input_dir)
+
+# python data_modifier_merge_packets.py --input_dir  --output_path  --label dns2tcp --mode csv
